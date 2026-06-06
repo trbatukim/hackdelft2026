14dd6a302fd0c29e6b0b9d9c8f7757619bca923f
Batuhan Özkara <b.ozkara@student.tudelft.nl>
2026-06-06 14:09:18 +0200

    added main

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String original = "Hello world!";
+        int key = 42;
+
+        String encrypted = encrypt(original, key);
+
+        int input = 0;
+        boolean valid = false;
+
+        while (!valid){
+            try {
+                input = sc.nextInt();
+                valid = true;
+                sc.close();
+            } catch (Exception e) {
+                valid = false;
+                sc.next();
+            }
+        }
+
+        String decrypted = decrypt(encrypted, input);
+
+        System.out.println("Original:  " + original);
+        System.out.println("Encrypted: " + encrypted);
+        System.out.println("Decrypted: " + decrypted);
+    }
+
+    public static String encrypt(String text, int key) {
+        StringBuilder result = new StringBuilder();
+        for (char c : text.toCharArray()) {
+            result.append((char) (c ^ key));
+        }
+        return result.toString();
+    }
+
+    public static String decrypt(String encrypted, int key) {
+        return encrypt(encrypted, key); // Same operation!
+    }
+}
